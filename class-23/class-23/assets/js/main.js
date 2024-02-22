f0b189202394b4cd03e3a991fe77a6e4127ec617
Nazmul Hossain Pappu <mdpappu006@gmail.com>
2024-02-22 19:33:16 +0600

    class-23

---
+++ class-23/class-23/assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+    // Write your Jquery code here
+
+    $(".box").css({"background":"#2196F3", " border-radius":"5px" , "padding":"15px", "width": "35%"});
+
+
+
+    // $(".hide").click(function(){
+    //     $(".box").css("opacity","0");
+    // });
+
+    // $(".show").click(function(){
+    //     $(".box").css("opacity","100%");
+    // });
+
+    // $(".box").dblclick(function(){ 
+
+    //     $(".box").css("display","none");
+
+    // });
+
+
+    // $(".box").contextmenu(function(){ 
+
+    //     $(".box").css("display","none");
+
+    // });
+
+
+
+    // $(".box").mouseenter(function(){ 
+
+    //     $(".box").css("display","none");
+
+    // });
+
+
+    $(".box").mouseleave(function(){ 
+
+        // $(".box").css("display","none");
+
+    });
+
+
+
+
+
+});
